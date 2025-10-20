dfb3deab6298df44627344f0bc4d43ea2adfb23f
Jonas Grubbe <grubbe.p@hotmail.com>
2025-10-20 10:06:25 +0200

    Debug

--- ProductCatalogMS/obj/Debug/net8.0/ProductCatalogMS.AssemblyInfo.cs
+++ ProductCatalogMS/obj/Debug/net8.0/ProductCatalogMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductCatalogMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e75b5cd9739e826f3871201b6ebe9d0af493021f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16daac147ab3fb5b17c65bfb2cd492121887955d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductCatalogMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductCatalogMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
